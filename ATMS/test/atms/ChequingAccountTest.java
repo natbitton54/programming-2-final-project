2d96307fe87cfaeba54078dec9dc4a52074bba84
natbitton54 <84545346+natbitton54@users.noreply.github.com>
2023-12-05 22:14:35 -0500

    Add files via upload

---
+++ ATMS/test/atms/ChequingAccountTest.java
@@ -0,0 +1,19 @@
+package atms;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class ChequingAccountTest extends TestCase {
+
+    @Test
+    public void testWithdrawValidAmount() {
+        // Arrange
+        ChequingAccount account = new ChequingAccount(1, 1000.0);
+
+        // Act
+        account.withdraw(500.0);
+
+        // Assert
+        assertEquals(500.0, account.getBalance());
+    }
+}
